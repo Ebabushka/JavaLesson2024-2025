5286ee46ee312fc5f1bc43e1ec39a7fdae267232
Olejan <oadmirtiriev13@gmail.com>
2024-08-28 22:01:08 +0300

    Задание №8: выполнено.

---
+++ src/main/java/org/example/Human/Document.java
@@ -0,0 +1,29 @@
+package org.example.Human;
+
+public class Document {
+    String nameDocument;
+    Integer number;
+    Integer series;
+
+    public Document(String nameDocument, Integer number, Integer series) {
+        this.nameDocument = nameDocument;
+        this.number = number;
+        this.series = series;
+    }
+
+    public String getNameDocument() {
+        return nameDocument;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Integer getSeries() {
+        return series;
+    }
+
+    public String document() {
+        return String.format("\nНаименование документа: %s; \nНомер документа: %s; \nСерия документа: %s.", nameDocument, number.toString(), series.toString());
+    }
+}
