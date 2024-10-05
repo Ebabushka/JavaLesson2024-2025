c237edeb4c56c7173d41cd31daea1b9eae3454ae
Olejan <oadmirtiriev13@gmail.com>
2024-10-05 18:44:04 +0300

    Замечания учтены #3.5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -7,7 +7,6 @@ public class App {
     private static Logger logger = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) {
-
         Person person1 = new Person("Иванов Иван Иванович", "05.11.1990", "+78005553535", "Голубой", "Уфа");
         Person person2 = new Person("Иванова Ирина Ивановна", "24.05.1980", "+783405553535", "Розовый", "Самара");
         logger.info(String.format("%s", person1.equals(person2)));
